b390e545dd4fdcdabab8369574de2ab74f2d01d1
SKatiyar <katiyar.suyash@gmail.com>
2014-12-13 15:15:34 +0530

    markus bookmark search

--- client/app/scripts/background.js
+++ client/app/scripts/background.js
@@ -1,25 +1,39 @@
 'use strict';
 
-var app;
-
-// chrome event listners
-chrome.runtime.onInstalled.addListener(function (details) {
-  app = Markus.init();
-});
-
-chrome.bookmarks.onCreated.addListener(function (id, details) {
-  console.log('\'Allo \'Allo! Event Page', details);
-});
-
 // omnibox event listners
 chrome.omnibox.setDefaultSuggestion({
   description: ' %s'
 });
 
-chrome.omnibox.onInputStarted.addListener(function() {});
+// chrome event listners
+chrome.runtime.onInstalled.addListener(function (details) {
+  var app = Markus();
 
-chrome.omnibox.onInputChanged.addListener(function() {});
+  chrome.omnibox.onInputChanged.addListener(function(text, suggest) {
+    if (text.length > 4) {
+      var url = 'http://google.com';
+      var title = 'Google Search';
+      var ole = app.debounce(function() {
+        chrome.bookmarks.search(text, function(k) {
+          var suggestions = [];
+          for (var i = 0; i < k.length; i++) {
+            suggestions.push({
+              content: k[i].url,
+              description: k[i].title + ' - ' + k[i].url
+            });
+          }
+          suggest(suggestions);
+        });
+      }, 300);
+      ole();
+    }
+  });
 
-chrome.omnibox.onInputEntered.addListener(function() {});
+  chrome.omnibox.onInputEntered.addListener(function(url) {
+    chrome.tabs.getSelected(null, function(tab){
+      chrome.tabs.update(tab.id, {url: url});
+    });
+  });
 
-chrome.omnibox.onInputCancelled.addListener(function() {});
+  chrome.omnibox.onInputCancelled.addListener(function() {});
+});
--- client/app/scripts/markus.js
+++ client/app/scripts/markus.js
@@ -1,23 +1,54 @@
 'use strict';
 
-var Markus = {
-  _bookmarks: []
-  init: function() {
-    var that = this;
-    chrome.bookmarks.getTree(function(data) {
-      var arr = [];
-      that.traverseTree(data[0], arr);
-    });
-  },
-  traverseTree: function(data, barr) {
-    if (typeof data === 'object') {
-      console.log(data);
-    } else if (typeof data === 'array') {
-      console.log('-------', data);
-    }
-  },
-  sendData: function() {
-  },
-  search: function(term) {
-  }
-};
+(function(w) {
+  w.Markus = w.Markus || function() {
+    var _bbox = {};
+
+    _bbox.bookmark = {
+      url:""
+    };
+    _bbox.bookmarks = [];
+    _bbox.init = function() {
+    };
+    _bbox.traverseTree = function() {
+    };
+    _bbox.sendData = function() {
+    };
+    _bbox.search = function(term) {
+    };
+
+    _bbox.debounce = function(func, wait, immediate) {
+      var timeout, args, context, timestamp, result;
+
+      var later = function() {
+        var last = Date.now() - timestamp;
+
+        if (last < wait && last > 0) {
+          timeout = setTimeout(later, wait - last);
+        } else {
+          timeout = null;
+          if (!immediate) {
+            result = func.apply(context, args);
+            if (!timeout) context = args = null;
+          }
+        }
+      };
+
+      return function() {
+        context = this;
+        args = arguments;
+        timestamp = Date.now();
+        var callNow = immediate && !timeout;
+        if (!timeout) timeout = setTimeout(later, wait);
+        if (callNow) {
+          result = func.apply(context, args);
+          context = args = null;
+        }
+
+        return result;
+      };
+    };
+
+    return _bbox;
+  };
+})(window);
